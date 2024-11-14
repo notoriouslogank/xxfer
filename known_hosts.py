a735e5d0870756f2fea201dc9554f26323029eb0
logank <logan.aker1989@gmail.com>
2024-11-14 12:41:55 -0500

    got known_hosts_add working as expected

--- known_hosts.py
+++ known_hosts.py
@@ -1,11 +1,13 @@
-from rich import print_json
 import json
 
-hosts = []
-known_hosts = []
+from rich import print_json
+
+hosts = []  # TODO: Change the names for these functions
+known_hosts = []  # TODO: Change the names for these functions
 
 
 class Host:
+    """Class for identifying and saving user-submitted known-hosts"""
 
     def __init__(self, name: str, ip: str, port: int | str):
         self.name = name
@@ -13,13 +15,27 @@ class Host:
         self.port = port
 
     def show_host(self):
+        """Pretty prints the entered information in a human-readable way way." """
         print(f"Hostname: {self.name}, IP: {self.ip}, PORT: {self.port}")
 
     def new_host(self):
+        """Create a dict for the provided host information.
+
+        Returns:
+            host (dict): Name, IP, and Port for the remote host.
+        """
         host = {"name": self.name, "ip": self.ip, "port": self.port}
         return host
 
     def make_json(self, data):
+        """Return a json object containing the host information
+
+        Args:
+            data (_type_): Input data (host information) to transform into JSON
+
+        Returns:
+            json_data (str): JSON formatted input data containing host information
+        """
         json_data = json.dumps(data, indent=3)
         return json_data
 
@@ -30,23 +46,16 @@ ports = ["5001", "5002", "5002"]
 
 
 def make_hostnames():
-
-    for i in range(len(names)):
+    """Print JSON-encoded known hosts to terminal (eventually to JSON file as well)"""
+    i = len(names) - 1
+    while i >= 0:
         data = [names[i], ips[i], ports[i]]
         host = Host(data[0], data[1], data[2])
         host_data = host.new_host()
-        host.make_json(host_data)
-        print_json(host.make_json(host_data))
+        json_hostnames = host.make_json(host_data)
+        print_json(json_hostnames)
+        i -= 1
+        # TODO: Write this to JSON file
 
 
-#        print(len(known_hosts
 make_hostnames()
-# print_json(known_hosts)
-# print_json(make_hostnames())
-
-# host1.new_host()
-# host2.new_host()
-# host1.make_json()
-# host2.make_json()
-# for i in known_hosts:
-#    print_json(i)
