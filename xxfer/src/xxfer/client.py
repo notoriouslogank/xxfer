99682b042c2c74808e637a06148af8746d701693
logank <logan.aker1989@gmail.com>
2024-11-15 18:44:31 -0500

    restructured project

---
+++ xxfer/src/xxfer/client.py
@@ -0,0 +1,133 @@
+import logging
+import os
+import socket
+
+import tqdm
+import yaml
+from prompt_toolkit import PromptSession
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.output import ColorDepth
+from prompt_toolkit.styles import Style
+from rich import print
+
+from packer import Compressor
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s::%(levelname)s::Line %(lineno)s\n%(message)s"
+)
+file_handler = logging.FileHandler("xfer.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+
+with open("config.yml", "r") as yml:
+    configs = yaml.safe_load(yml)
+
+settings = configs["settings"]
+ARCHIVE_NAME = settings["archive"]
+CURRENT_DIR = os.getcwd()
+SEPARATOR = settings["separator"]
+BUFFER_SIZE = settings["buffer_size"]
+
+
+class Client:
+    known_hosts = ""
+    style = Style.from_dict(
+        {
+            "completion-menu.completion": "bg:#008888 #ffffff",
+            "completion-menu.completion.current": "bg:#00aaaa #000000",
+            "scrollbar.background": "bg:#88aaaa",
+            "scrollbar.button": "bg:#222222",
+        }
+    )
+    autocompleter = WordCompleter([known_hosts])
+
+    def get_known_hosts(self):
+
+        host_list = []
+        for _ in configs["known_hosts"]:
+            host_list.append(_)
+        length = len(self.known_hosts)
+        counter = length
+        while counter > 0:
+            self.known_hosts.join(host_list.pop())
+            counter -= 1
+        return self.known_hosts
+
+    def get_server_info(self):
+        session = PromptSession(
+            completer=self.autocompleter,
+            style=self.style,
+            color_depth=ColorDepth.TRUE_COLOR,
+        )
+        try:
+            remote_ip = session.prompt("Remote Server IP\n>> ")
+            remote_port = session.prompt("Remote Server Port\n>> ")
+            if str(remote_ip) in self.known_hosts:
+                remote_ip = configs["known_hosts"][remote_ip]["ip"]
+                remote_port = configs["known_hosts"][remote_ip]["port"]
+                self.host_ip = remote_ip
+                self.host_port = remote_port
+                return self.host_ip, self.host_port
+            elif str(remote_ip) not in self.known_hosts:
+                self.host_ip = remote_ip
+                self.host_port = int(remote_port)
+                return self.host_ip, self.host_port
+        except KeyboardInterrupt:
+            pass
+        else:
+            print(f"[+] Remote Host: {self.host_ip}:{self.host_port}")
+
+    def send(self, host, port, filename=None):
+
+        if filename == None:
+            session = PromptSession(style=self.style, color_depth=ColorDepth.TRUE_COLOR)
+            source_directory = os.path.join(
+                session.prompt(
+                    "File/directory to send\n[Absolute path unless in .]\n>> "
+                )
+            )
+        else:
+            source_directory = os.path.join(filename)
+        compressed_file = Compressor.compress(ARCHIVE_NAME, source_directory)
+        filesize = os.path.getsize(compressed_file)
+        os.chdir(CURRENT_DIR)
+        s = socket.socket()
+        print(f"[+] Connecting to {host}:{port}...\n")
+        s.connect((host, port))
+        print(f"[+] Connected!\n")
+        s.send(f"{ARCHIVE_NAME}{SEPARATOR}{filesize}".encode())
+
+        progress = tqdm.tqdm(
+            range(filesize),
+            f"Sending {compressed_file}",
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+        )
+
+        with open(compressed_file, "rb") as f:
+            while True:
+                bytes_read = f.read(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                s.sendall(bytes_read)
+                progress.update(len(bytes_read))
+
+
+def populate_known_hosts(known_hosts: list, length: int):
+    counter = length
+    host_list = []
+    while counter > 0:
+        host_list.append(known_hosts[counter])
+        counter -= 1
+    print(host_list)
+
+
+if __name__ == "__main__":
+    while True:
+        client = Client()
+        host, port = client.get_server_info()
+        client.send(host, int(port))
---
+++ xxfer/src/xxfer/server.py
@@ -0,0 +1,88 @@
+import logging
+import os
+import socket
+import sys
+
+import tqdm
+import yaml
+from rich import print
+
+from packer import Compressor
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s::%(levelname)s:%(message)s")
+file_handler = logging.FileHandler("xfer.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+try:
+    with open("config.yml", "r") as yml:
+        configs = yaml.safe_load(yml)
+except FileNotFoundError:
+    logger.error("No config.yml found")
+    pass
+else:
+    logger.info("Loaded config.yml")
+
+settings = configs["settings"]
+known_hosts = configs["known-hosts"]
+
+SEPARATOR = settings["separator"]
+BUFFER_SIZE = settings["buffer_size"]
+SERVER_HOST = settings["host"]
+SERVER_PORT = settings["port"]
+ARCHIVE_NAME = settings["archive"]
+
+
+class RemoteHost:
+
+    def __init__(self):
+        self.host = SERVER_HOST
+        self.port = SERVER_PORT
+
+    def receive(self):
+        print(f"Now listening at {self.host}:{self.port}")
+        try:
+            s = socket.socket()
+            s.bind((self.host, self.port))
+            s.listen(5)
+            client_socket, address = s.accept()
+            received_data = client_socket.recv(BUFFER_SIZE).decode()
+            filename, filesize = received_data.split(SEPARATOR)
+            logging.debug(f"Received file {filename}{SEPARATOR}{int(filesize)/1024}")
+            filename = os.path.basename(filename)
+            filesize = int(filesize)
+            progress = tqdm.tqdm(
+                range(filesize),
+                f"[+] Receiving {filename}...",
+                unit="B",
+                unit_scale=True,
+                unit_divisor=1024,
+            )
+            with open(filename, "wb") as f:
+                while True:
+                    bytes_read = client_socket.recv(BUFFER_SIZE)
+                    if not bytes_read:
+                        break
+                    f.write(bytes_read)
+                    progress.update(len(bytes_read))
+        #                client_socket.close()
+        #                s.close()
+        except (KeyboardInterrupt, UnboundLocalError):
+            pass
+        try:
+            client_socket.close()
+            s.close()
+        except UnboundLocalError:
+            sys.exit()
+
+
+if __name__ == "__main__":
+    server = RemoteHost()
+    while True:
+        try:
+            server.receive()
+            Compressor.unpack(ARCHIVE_NAME)
+        except KeyboardInterrupt:
+            exit()
