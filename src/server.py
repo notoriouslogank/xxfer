b75733ed87bf7213b5b90173790f4318fc86d53c
logank <logan.aker1989@gmail.com>
2024-11-14 22:57:58 -0500

    trying to make receive work

--- src/server.py
+++ src/server.py
@@ -67,7 +67,7 @@ class RemoteHost:
                         break
                     f.write(bytes_read)
                     progress.update(len(bytes_read))
-                    client_socket.close()
+            client_socket.close()
         #                client_socket.close()
         #                s.close()
         except KeyboardInterrupt:
