3ceb553fe184661f1f6d8ef27dc9fbae9b868741
logank <logan.aker1989@gmail.com>
2024-11-25 10:19:25 -0500

    Docstrings for all

--- src/constants.py
+++ src/constants.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class Constants:
+
     installer = Installer()
     hostsfile = KnownHosts()
 
@@ -33,6 +34,11 @@ class Constants:
         self.ARCHIVE_NAME = settings["ARCHIVE_NAME"]
 
     def get_filepaths(self) -> pathlib.Path:
+        """Get paths for default directories relative to platform
+
+        Returns:
+            pathlib.Path: user_download_dir, user_data_dir, user_config_dir, user_log_dir
+        """
         user_config_dir = platformdirs.user_config_path(self.APP_NAME)
         user_log_dir = platformdirs.user_log_path(self.APP_NAME)
         user_data_dir = platformdirs.user_data_path(self.APP_NAME, self.APP_AUTHOR)
@@ -43,16 +49,31 @@ class Constants:
         return user_data_dir, user_config_dir, user_log_dir, user_download_dir
 
     def find_data_dir(self) -> pathlib.Path:
+        """Get path for user data directory
+
+        Returns:
+            pathlib.Path: User data directory
+        """
         user_data_dir, _, _, _ = self.get_filepaths()
         logging.info(f"DATA_DIR = {user_data_dir}")
         return user_data_dir
 
     def find_download_dir(self) -> pathlib.Path:
+        """Get user download directory
+
+        Returns:
+            pathlib.Path: Get path for user download directory
+        """
         _, _, _, user_download_dir = self.get_filepaths()
         logging.debug(f"DONLOAD_DIR = {user_download_dir}")
         return user_download_dir
 
     def find_configfile(self) -> pathlib.Path:
+        """Get path to user configuration file
+
+        Returns:
+            pathlib.Path: User configuration file
+        """
         _, user_config_path_stem, _, _ = self.get_filepaths()
         config_file_name = "config.yml"
         config_file = pathlib.Path.joinpath(user_config_path_stem, config_file_name)
@@ -60,6 +81,11 @@ class Constants:
         return config_file
 
     def find_logfile(self) -> pathlib.Path:
+        """Get path to default system log file
+
+        Returns:
+            pathlib.Path: Logfile
+        """
         _, _, user_log_dir, _ = self.get_filepaths()
         logfile_name = f"{self.APP_NAME}.log"
         logfile = pathlib.Path.joinpath(user_log_dir, logfile_name)
