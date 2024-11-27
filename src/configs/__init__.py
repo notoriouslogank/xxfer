074a796db86feef2cc8ce30e4bce7b37528ff025
logank <logan.aker1989@gmail.com>
2024-11-27 09:24:38 -0500

    Moved configuration helper modules to their own package.

---
+++ src/configs/__init__.py
@@ -0,0 +1,11 @@
+import logging
+
+from constants import Constants
+
+constants = Constants("xxfer", "notoriouslogank")
+LOGFILE = constants.LOGFILE
+APP_NAME = constants.APP_NAME
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:Line %(lineno)s\n%(message)s")
+file_handler = logging.FileHandler(f"{LOGFILE}", "a")
---
+++ src/configs/knownhosts.py
@@ -0,0 +1,29 @@
+import logging
+import pathlib
+
+import platformdirs
+import yaml
+
+logger = logging.getLogger(__name__)
+APP_NAME = "xxfer"
+
+
+class KnownHosts:
+
+    host_data = {"HOST": "0.0.0.0", "PORT": 5002}
+    known_hostnames = {"localhost": host_data}
+    data = {"KNOWN_HOSTS": known_hostnames}
+    dir = platformdirs.user_config_path(APP_NAME)
+    name = "hosts.yml"
+
+    def __init__(self):
+        self.HOSTSFILE = pathlib.Path.joinpath(self.dir, self.name)
+
+    def write(self, hostsfile):
+        if hostsfile:
+            logger.info(f"Hostsfile already exists: {hostsfile}")
+            pass
+        else:
+            with open(hostsfile, "w") as file:
+                logger.debug(f"Writing hostsfile: {hostsfile}")
+                yaml.dump(self.data, file, default_flow_style=False)
