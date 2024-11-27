b9defca5113fac83e5d4e79a14812de57c8317fb
logank <logan.aker1989@gmail.com>
2024-11-27 09:24:38 -0500

    Moved configuration helper modules to their own package.

---
+++ src/configs/configfile.py
@@ -0,0 +1,39 @@
+import logging
+import os
+import pathlib
+
+import platformdirs
+import yaml
+
+logger = logging.getLogger(__name__)
+APP_NAME = "xxfer"
+
+
+class ConfigFile:
+
+    settings = {
+        "SEPARATOR": "<SEPARATOR>",
+        "BUFFER_SIZE": 4096,
+        "HOST": "0.0.0.0",
+        "PORT": 5002,
+        "DOWNLOAD_DIR": "xxfer_received",
+        "ARCHIVE_NAME": "xxfer.tar.gz",
+    }
+    data = {"SETTINGS": settings}
+    dir = platformdirs.user_config_path(APP_NAME)
+    name = "config.yml"
+
+    def __init__(self):
+        self.CONFIGFILE = pathlib.Path.joinpath(self.dir, self.name).resolve()
+
+    def make_config_directory(self):
+        if self.CONFIGFILE.exists() == False:
+            logger.debug(f"Created config directory: {self.dir}")
+            os.makedirs(self.dir, exist_ok=True)
+        else:
+            pass
+
+    def write(self, configfile):
+        with open(configfile, "w") as file:
+            logger.debug(f"Wrote config file: {configfile}")
+            yaml.dump(self.data, file, default_flow_style=False)
---
+++ src/configs/logfile.py
@@ -0,0 +1,24 @@
+import logging
+import os
+import pathlib
+
+import platformdirs
+
+logger = logging.getLogger(__name__)
+APP_NAME = "xxfer"
+
+
+class LogFile:
+
+    logname = f"{APP_NAME}.log"
+    dir = platformdirs.user_log_path(APP_NAME)
+
+    def __init__(self):
+        self.LOGFILE = pathlib.Path.joinpath(self.dir, self.logname).resolve()
+
+    def make_log_directory(self):
+        if self.LOGFILE.exists() == False:
+            logger.debug(f"Making Log directory: {self.dir}")
+            os.makedirs(self.dir, exist_ok=True)
+        else:
+            pass
